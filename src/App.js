19325c5a6b8a12c40bed42d84d9fc88a37f46203
Nilaxi11 <nilaxi1109@gmail.com>
2026-05-29 12:19:22 +0530

    updated app.jsx

--- src/App.js
+++ src/App.js
@@ -2,13 +2,11 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import RecipeList from "./Components/RecipeList";
 import RecipeDetail from "./Components/RecipeDetails";
-import Recipe from "./Components/Recipe";
 
 function App() {
   return (
     <Router>
       <Routes>
-        {/* <Route path="/" element={<Recipe />} /> */}
         <Route path="/" element={<RecipeList />} />
         <Route path="/recipe/:id" element={<RecipeDetail />} />
       </Routes>
